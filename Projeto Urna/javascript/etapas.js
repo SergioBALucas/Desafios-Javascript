af520237145a5b1b98c5b3c76697ce0693e7df2d
SolemnReign <sirgiooo@hotmail.com>
2021-09-14 22:50:51 -0300

    urna

---
+++ Projeto Urna/javascript/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '13024',
+                nome: 'Gustavo Inácio Lula da Silva',
+                partido: 'PT',
+                fotos: [
+                    {url:'../imagens/gustavo.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '42069',
+                nome: 'Pedro da Silva Vaz de Lima',
+                partido: 'REDE',
+                fotos: [
+                    {url:'../imagens/pedro.png', legenda: 'Vereador'}
+                ] 
+            },
+        ]
+    }, 
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '10',
+                nome: 'Jailson Costa dos Santos',
+                partido: 'PROA',
+                vice: 'Barack Hussein Obama II',
+                fotos: [
+                    {url:'../imagens/presidente.jpeg', legenda: 'Prefeito'},
+                    {url:'../imagens/obama.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Jair Messias Bolsonaro',
+                partido: 'RUIM',
+                vice: 'Donald John Trump',
+                fotos: [
+                    {url:'../imagens/bonoro.jpg', legenda: 'Prefeito'},
+                    {url:'../imagens/trampo.jpeg', legenda: 'Vice-Prefeito', small: true}
+                ]  
+            },
+        ]
+    }
+];
